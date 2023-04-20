7279121b6126ec251af9b2af180dc51b4448910b
jordan <jharrison1@qa.com>
2023-04-20 13:03:20 +0100

    integration tests

--- src/test/java/com/qa/demo/integration/CatIntegrationTest.java
+++ src/test/java/com/qa/demo/integration/CatIntegrationTest.java
@@ -1,7 +1,13 @@
 package com.qa.demo.integration;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +22,7 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.qa.demo.domain.Cat;
+import com.qa.demo.domain.Cat;;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -44,13 +50,27 @@ public class CatIntegrationTest {
 	}
 
 	@Test
-	void testCreate2() throws Exception {
-		Cat newCat = new Cat("Chairman Meow", true, true, 12);
-		String newCatAsJson = this.mapper.writeValueAsString(newCat);
-
-		Cat created = new Cat(2, "Chairman Meow", true, true, 12);
-		String createdAsJson = this.mapper.writeValueAsString(created);
-		this.mvc.perform(post("/create").content(newCatAsJson).contentType(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isCreated()).andExpect(content().json(createdAsJson));
+	void testGet() throws Exception {
+		this.mvc.perform(get("/get/1")).andExpect(status().isOk()).andExpect(
+				content().json(this.mapper.writeValueAsString(new Cat(1, "Mr Bigglesworth", true, true, 27))));
 	}
+
+	@Test
+	void testGetAll() throws Exception {
+		this.mvc.perform(get("/getAll")).andExpect(status().isOk()).andExpect(
+				content().json(this.mapper.writeValueAsString(List.of(new Cat(1, "Mr Bigglesworth", true, true, 27)))));
+	}
+
+	@Test
+	void testDelete() throws Exception {
+		this.mvc.perform(delete("/remove/1")).andExpect(status().isOk()).andExpect(
+				content().json(this.mapper.writeValueAsString(new Cat(1, "Mr Bigglesworth", true, true, 27))));
+	}
+
+	@Test
+	void testUpdate() throws Exception {
+		this.mvc.perform(patch("/update/1").queryParam("name", "tiddles")).andExpect(status().isOk())
+				.andExpect(content().json(this.mapper.writeValueAsString(new Cat(1, "tiddles", true, true, 27))));
+	}
+
 }
